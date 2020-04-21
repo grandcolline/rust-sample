d0cc70dbfedd7be02a40b2c18aa0b8f74aebf322
Taiki Hayashi <grandcolline@gmail.com>
2020-04-22 01:14:15 +0900

    add proto

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,6 @@ impl Greeter for MyGreeter {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let addr = "[::1]:50051".parse().unwrap();
     let addr = "0.0.0.0:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
